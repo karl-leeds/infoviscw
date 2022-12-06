fb6fa2ad19f8847c5640dbfa3b759105f218831a
karl-leeds <117841035+karl-leeds@users.noreply.github.com>
2022-12-06 20:22:12 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import matplotlib as matplotlib
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import squarify
+
+
+#Displaying the 10 questions for each of the 10 bar charts and maps.
+for i in range(20):
+  
